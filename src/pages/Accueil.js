cfae508fc9dd502c4d9d15666d081b6c99263420
Alexandru Ghilev <aghilev@yahoo.fr>
2022-06-29 09:35:50 +0200

    Initialisation projet frontend

---
+++ src/pages/Accueil.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accueil = () => {
+    return (
+        <div>
+            Hello depuis composant Accueil !
+        </div>
+    );
+};
+
+export default Accueil;
